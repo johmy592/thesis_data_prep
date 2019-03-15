0b99b381fbe1e69413f4adcbe5f9c84d3b18c742
johmy592 <johmy592@student.liu.se>
2019-03-15 10:41:52 +0100

    Added scripts for all notebooks

---
+++ scripts/concatenate_corpus.py
@@ -0,0 +1,39 @@
+import sys
+
+
+def concatenate_files(filename1, filename2, write_filename):
+    f1 = open(filename1,'r')
+    f2 = open(filename2,'r')
+    wf = open(write_filename,'w+')
+    line_num = 0
+    for line in f1:
+        line_num += 1
+        wf.write(line)
+        if((line_num % 10000000) == 0):
+            print("Wrote ",str(line_num),"lines\n")
+    f1.close()
+    print("First file done!\n")
+    line_num = 0
+    for line in f2:
+        line_num += 1
+        wf.write(line)
+        if((line_num % 10000000) == 0):
+            print("Wrote ",str(line_num),"lines\n")
+        wf.write(line)
+    print("Wrote ", str(line_num), "lines from second file\n")
+    f2.close()
+    wf.close()
+
+
+if __name__ == "__main__":
+
+    if(len(sys.argv) != 4):
+        print("Usage:")
+        print("python3 concatenate_corpus.py <corpus_path1> <corpus_path2> <save_path>")
+        sys.exit()
+
+    first_corpus = sys.argv[1]
+    second_corpus = sys.argv[2]
+    save_file = sys.argv[3]
+
+    concatenate_files(first_corpus, second_corpus, save_file) 
---
+++ scripts/make_term_corpus.py
@@ -0,0 +1,109 @@
+import sys
+import spacy
+from spacy.tokenizer import Tokenizer
+from os import listdir
+from os.path import isfile, join
+
+
+nlp = spacy.load('en', disable=['tagging','parser','ner'])
+nlp.add_pipe(nlp.create_pipe('sentencizer'))
+
+IGNORE_TOKENS = ['༔','༅','༒','༖','༗','']
+DIVIDERS = ['༎']
+IGNORE_WHOLE_LINE = ['\\']
+
+
+def get_filenames(files_dir):
+    return [f for f in listdir(files_dir) if isfile(join(files_dir, f))]
+
+
+def replace_div_chars(filename):
+    tf = open(filename,'r')
+    texts_list = []
+    for line in tf:
+        ignore_line=False
+        for ic in IGNORE_WHOLE_LINE:
+            if ic in line:
+                ignore_line=True
+        if ignore_line:
+            continue 
+        for div in DIVIDERS:
+            line = line.replace(div,'\n')
+        clean_line = line.split('\n')
+        texts_list += [l for l in clean_line if l]
+    return texts_list
+
+
+def extract_text(texts):
+    texts_list = []
+    for line in texts:
+        ignore_line=False
+        for ic in IGNORE_WHOLE_LINE:
+            if ic in line:
+                ignore_line=True
+        if ignore_line:
+            continue
+        clean_line = ""
+        for c in line:
+            if c in IGNORE_TOKENS:
+                if clean_line.strip('\n').strip(' '):
+                    texts_list.append(clean_line.replace(u'\xa0', u' ').strip('\n'))
+                    clean_line=""
+                continue
+            clean_line += c
+        if clean_line.strip('\n').strip(' '):
+            texts_list.append(clean_line.replace(u'\xa0', u' ').strip('\n'))
+    return texts_list
+
+
+def make_tokenized_doc_list(texts_list): 
+    return [nlp(line) for line in texts_list]
+
+
+def write_tokenized_text_to_file(doc_list, file_path):
+    fp = open(file_path,'w+')
+    for doc in doc_list:
+        sentences = doc.sents
+        for s in sentences:
+            line = ""
+            for t in s:
+                if t.text in [' ','\t']:
+                    continue
+                line += t.text
+                if t.text != '\n':
+                    line += ' '
+            if line.strip(' ').strip('\n'):
+                fp.write(line)
+                fp.write('\n')
+    fp.close()
+
+
+def aggregate_all(files, location):
+    intermediate = []
+    all_lines = []
+    print('Extracting lines from ',len(files), 'files\n')
+    
+    for filename in files:
+        intermediate += replace_div_chars(texts_dir+filename)
+    
+    all_lines = extract_text(intermediate)
+    print('Tokenizing ', len(all_lines),'lines.')
+    print('This might take several minutes\n')
+    docs = make_tokenized_doc_list(all_lines)
+    print('Writing to file\n')
+    write_tokenized_text_to_file(docs, location)
+
+
+if __name__ == "__main__":
+    
+    if(len(sys.argv) != 3):
+        print("Usage: ")
+        print("python3 make_term_corpus.py <textst_dir> <save_path>")
+        sys.exit()
+
+    texts_dir = sys.argv[1]
+    save_location = sys.argv[2]    
+
+    files_list = get_filenames(texts_dir)
+    aggregate_all(files_list, save_location)
+
